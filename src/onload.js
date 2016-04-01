bf75b0fa5f75e9e468fccc29a806d04f1e2677ff
karolis <karolis.lukosiuse@netent.com>
2016-04-01 22:58:52 +0200

    tabs to spaces.... =)

---
+++ src/onload.js
@@ -0,0 +1,9 @@
+/*
+var url = "https://www.youtube.com/watch?v=AAfutMP3uEU";
+var videoE = '<video tabindex="-1" class="video-stream html5-main-video" style="width: 640px; height: 360px; left: 0px; top: 0px; transform: none; opacity: 1; position: fixed; top:32px; left:32px;" src="blob:https%3A//www.youtube.com/1abdb3f7-282b-4255-89e5-6a27c15d6ff7"></video>';
+var div = document.createElement('div');
+div.innerHTML = videoE;
+div.id = 'player';
+document.body.appendChild(div);
+
+*/
--- src/replaceThumbs.js
+++ src/replaceThumbs.js
@@ -1,23 +1,23 @@
 
 var replaceThumbs = function(){
-	var clearChildren = function(e){ while( e.hasChildNodes() ){ e.removeChild( e.lastChild ); } };
-	Array.prototype.forEach.call(document.querySelectorAll('.yt-lockup-thumbnail'), function(container){
-		var a = container.querySelector('a'), image, videoFrame;
-		if(a){
-			image = container.querySelector('img');
-			image.onclick = function(){
-				//videoFrame = '<iframe width='+c.clientWidth+' height='+c.clientHeight+' src="https://www.youtube.com/v/'+a.href.split('?v=')[1]+'?rel=0&autoplay=1">';
-				videoFrame = document.createElement('iframe');
-				videoFrame.width = container.clientWidth;
-				videoFrame.height = container.clientHeight;
+    var clearChildren = function(e){ while( e.hasChildNodes() ){ e.removeChild( e.lastChild ); } };
+    Array.prototype.forEach.call(document.querySelectorAll('.yt-lockup-thumbnail'), function(container){
+        var a = container.querySelector('a'), image, videoFrame;
+        if(a){
+            image = container.querySelector('img');
+            image.onclick = function(){
+                //videoFrame = '<iframe width='+c.clientWidth+' height='+c.clientHeight+' src="https://www.youtube.com/v/'+a.href.split('?v=')[1]+'?rel=0&autoplay=1">';
+                videoFrame = document.createElement('iframe');
+                videoFrame.width = container.clientWidth;
+                videoFrame.height = container.clientHeight;
                 videoFrame.id = 'player';
-				videoFrame.src = 'https://www.youtube.com/v/${videoId}?rel=0&autoplay=1'.replace('${videoId}', a.href.split('?v=')[1]);
-				clearChildren(container);
-				container.appendChild(videoFrame); 
-			};
-			clearChildren(container);
-			container.appendChild( image );
-		}
-	});
+                videoFrame.src = 'https://www.youtube.com/v/${videoId}?rel=0&autoplay=1'.replace('${videoId}', a.href.split('?v=')[1]);
+                clearChildren(container);
+                container.appendChild(videoFrame);
+            };
+            clearChildren(container);
+            container.appendChild( image );
+        }
+    });
 };
 replaceThumbs();
